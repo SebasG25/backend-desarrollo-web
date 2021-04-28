64ab91a5b29acf8a69d17fc856e80a2db9ac35b1
SebasG25 <bitas200225@gmail.com>
2021-04-28 14:57:02 -0500

    - Corrección de errores de nodemailer

--- app/services/nodemailer.service.js
+++ app/services/nodemailer.service.js
@@ -5,7 +5,7 @@ class NodemailerService{
         this.mailOptions = {
             from: 'prueba.backend25@gmail.com',
             to: '',
-            subject: `Bienvenido(a) `,
+            subject: '',
             text: 'mensaje',
             html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "https://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
             <html xmlns="http://www.w3.org/1999/xhtml">
@@ -56,7 +56,7 @@ class NodemailerService{
 
     async enviarCorreo(destinatario, nombre, cuerpo){
         this.mailOptions.to = destinatario;
-        this.mailOptions.subject += `${nombre}`;
+        this.mailOptions.subject =`Bienvenido ${nombre}`;
         this.mailOptions.html = cuerpo;
         let result = await this.transporter.sendMail(this.mailOptions);
         return result;
